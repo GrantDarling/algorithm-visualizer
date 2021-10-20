81b5a0463a38468f90a4e32679a31f2fb8fb9d6c
Grant Darling <grantcarterdarling@gmail.com>
2021-10-20 18:17:51 -0400

    refactored unnecessary code in grid

--- src/Components/Map/grid.tsx
+++ src/Components/Map/grid.tsx
@@ -1,6 +1,5 @@
 import { SortingGrid } from "../Algorithmns/Sorting/grid";
 import { createGrid } from "./logic/grid";
-import { useEffect, useState } from "react";
 
 export type GridProps = {
   rows: number;
@@ -8,12 +7,6 @@ export type GridProps = {
 };
 
 export const Grid = ({ columns, rows }: GridProps) => {
-  //let [grid, setGrid] = useState<number[][]>([]);
   let grid: number[][] = createGrid(columns, rows);
-
-  // useEffect(() => {
-  //   setGrid(createGrid(columns, rows));
-  // }, [columns, rows]);
-
   return <SortingGrid grid={grid} />;
 };
