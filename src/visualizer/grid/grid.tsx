7b1a6b33cf88b18d61615b5c195c54496c8c6c0b
Grant Darling <grantcarterdarling@gmail.com>
2021-10-27 23:22:38 -0400

    refactored grid.tsx

--- src/visualizer/grid/grid.tsx
+++ src/visualizer/grid/grid.tsx
@@ -1,22 +1,16 @@
 import { SortingGrid } from "./sorting-grid";
 import { createGrid } from "./grid-logic";
-
-// redux
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
 export const Grid = () => {
+  const global: IGlobal = useSelector((state: GlobalState) => state);
   const [grid, setGrid] = useState<number[][]>([]);
-  const global: IGlobal = useSelector((state: GlobalState) => {
-    return {
-      ...state,
-      gridSize: state.gridSize,
-    };
-  });
 
-  useEffect(() => {
-    setGrid(createGrid(global.gridSize, global.gridSize));
-  }, [global.gridSize]);
+  useEffect(
+    () => setGrid(createGrid(global.gridSize, global.gridSize)),
+    [global.gridSize]
+  );
 
   return <SortingGrid grid={grid} />;
 };
