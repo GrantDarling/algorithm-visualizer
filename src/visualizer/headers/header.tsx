a0a846c4ce59dd036fd552d954d8d4542bad1388
Grant Darling <grantcarterdarling@gmail.com>
2021-10-27 22:00:44 -0400

    refactored header.tsx

--- src/visualizer/headers/header.tsx
+++ src/visualizer/headers/header.tsx
@@ -2,30 +2,23 @@ import { CreateGrid } from "./create-grid";
 import { Complexity } from "./complexity";
 import { AlgorithmTypeSelector } from "./algorithm-type-selector";
 import { AlgorithmSelector } from "./algorithm-selector";
-
-// styles
 import { Head, SubHead, VisualizeButton } from "../../styles/styles";
 
-// redux
-import { toggleStart } from "../../store/actionCreators";
 import { Dispatch } from "redux";
 import { useDispatch, useSelector } from "react-redux";
+import { toggleStart } from "../../store/actionCreators";
 
 export const Header = () => {
   const dispatch: Dispatch<any> = useDispatch();
-  const start: IGlobal = useSelector((state: GlobalState) => {
-    return {
-      ...state,
-      start: !state.start,
-    };
-  });
+  const globalState = useSelector((state: GlobalState) => state);
+
   return (
     <>
       <Head>Algorithm Visualizer</Head>
       <SubHead>
         <AlgorithmTypeSelector />
         <AlgorithmSelector />
-        <VisualizeButton onClick={() => dispatch(toggleStart(start))}>
+        <VisualizeButton onClick={() => dispatch(toggleStart(globalState))}>
           visualize
         </VisualizeButton>
         <CreateGrid />
