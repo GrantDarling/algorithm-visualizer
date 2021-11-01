ebba984233b1ff8effe714bab4764d827e901eed
Grant Darling <grantcarterdarling@gmail.com>
2021-11-01 17:19:53 -0400

    added tests for algorithm type selector component

--- src/visualizer/headers/algorithm-type-selector.tsx
+++ src/visualizer/headers/algorithm-type-selector.tsx
@@ -2,12 +2,20 @@ import { AlgorithmTypeSelectorContainer } from "../../styles/styles";
 
 export const AlgorithmTypeSelector = () => {
   return (
-    <AlgorithmTypeSelectorContainer>
-      <label htmlFor="algorithm-type">Algorithm Type</label>
+    <AlgorithmTypeSelectorContainer data-test="algorithm-type-selector">
+      <label htmlFor="algorithm-type" data-test="algorithm-label">
+        Algorithm Type
+      </label>
       <select name="algorithm-type" id="algorithm-type">
-        <option value="sort">Sort</option>
-        <option value="search">Search</option>
-        <option value="graph">Graph</option>
+        <option value="sort" data-test="algorithm-option">
+          Sort
+        </option>
+        <option value="search" data-test="algorithm-option">
+          Search
+        </option>
+        <option value="graph" data-test="algorithm-option">
+          Graph
+        </option>
       </select>
     </AlgorithmTypeSelectorContainer>
   );
--- src/visualizer/headers/headers.test.tsx
+++ src/visualizer/headers/headers.test.tsx
@@ -1,34 +1,64 @@
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { shallow, configure } from "enzyme";
-import { AlgorithmSelector } from "./algorithm-selector";
-
 import { findByTestAttribute } from "../helpers/helpers";
+
+import { AlgorithmSelector } from "./algorithm-selector";
+import { AlgorithmTypeSelector } from "./algorithm-type-selector";
 
 configure({ adapter: new EnzymeAdapter() });
 
-const setUp = (props = {}) => {
-  const component = shallow(<AlgorithmSelector {...props} />);
-  return component;
-};
+describe("Header Components", () => {
+  describe("Algorithm Selector Component", () => {
+    const setUp = (props = {}) => {
+      const component = shallow(<AlgorithmSelector {...props} />);
+      return component;
+    };
 
-describe("Algorithm Selector Component", () => {
-  let wrapper: any;
-  beforeEach(() => {
-    wrapper = setUp();
+    let wrapper: any;
+    beforeEach(() => {
+      wrapper = setUp();
+    });
+
+    it("should render the component", () => {
+      const component = findByTestAttribute(wrapper, "algorithm-selector");
+      expect(component.length).toBe(1);
+    });
+
+    it("should render the label", () => {
+      const component = findByTestAttribute(wrapper, "algorithm-label");
+      expect(component.length).toBe(1);
+    });
+
+    it("should render the options", () => {
+      const component = findByTestAttribute(wrapper, "algorithm-option");
+      expect(component.length).toBe(3);
+    });
   });
 
-  it("should render the component", () => {
-    const component = findByTestAttribute(wrapper, "algorithm-selector");
-    expect(component.length).toBe(1);
-  });
+  describe("Algorithm Type Selector Component", () => {
+    const setUp = (props = {}) => {
+      const component = shallow(<AlgorithmTypeSelector {...props} />);
+      return component;
+    };
 
-  it("should render the label", () => {
-    const component = findByTestAttribute(wrapper, "algorithm-label");
-    expect(component.length).toBe(1);
-  });
+    let wrapper: any;
+    beforeEach(() => {
+      wrapper = setUp();
+    });
 
-  it("should render the options", () => {
-    const component = findByTestAttribute(wrapper, "algorithm-option");
-    expect(component.length).toBe(3);
+    it("should render the component", () => {
+      const component = findByTestAttribute(wrapper, "algorithm-type-selector");
+      expect(component.length).toBe(1);
+    });
+
+    it("should render the label", () => {
+      const component = findByTestAttribute(wrapper, "algorithm-label");
+      expect(component.length).toBe(1);
+    });
+
+    it("should render the options", () => {
+      const component = findByTestAttribute(wrapper, "algorithm-option");
+      expect(component.length).toBe(3);
+    });
   });
 });
