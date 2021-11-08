6de26afea9b3e395415b2d1993edeb92f03487bb
Grant Darling <grantcarterdarling@gmail.com>
2021-11-07 19:32:32 -0500

    changed grid size limit

--- src/visualizer/HeaderComponents/CreateGrid/create-grid-implementation.tsx
+++ src/visualizer/HeaderComponents/CreateGrid/create-grid-implementation.tsx
@@ -8,7 +8,7 @@ const useCreateGridImplementation = () => {
   const globalState = useSelector((state: GlobalState) => state);
   const [global] = useState(globalState);
   const [grid, setGrid] = useState(global.gridSize);
-  const [limit] = useState(70);
+  const [limit] = useState(50);
 
   useEffect(() => {
     dispatch(setGridSize(global, grid));
