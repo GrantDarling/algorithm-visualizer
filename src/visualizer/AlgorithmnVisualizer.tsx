6d45deaca383aca87f1c61de3aeb0b7192a1270c
Grant Darling <grantcarterdarling@gmail.com>
2021-11-07 19:51:58 -0500

    deleted yarn lock and added favicon + developer info

--- src/visualizer/AlgorithmnVisualizer.tsx
+++ src/visualizer/AlgorithmnVisualizer.tsx
@@ -2,12 +2,14 @@ import { Visualizer } from "../styles/styles-global";
 
 import { Header } from "./HeaderComponents/HeaderWrapper/header-presentational";
 import { Grid } from "./GridComponents/Grid/grid-presentational";
+import { Contact } from "./Contact";
 
 const AlgorithmnVisualizer = () => {
   return (
     <Visualizer>
       <Header />
       <Grid />
+      <Contact />
     </Visualizer>
   );
 };
