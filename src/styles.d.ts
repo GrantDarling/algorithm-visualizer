4e7e68541fa7fe3b9b251875955c8e58761df95c
Grant Darling <grantcarterdarling@gmail.com>
2021-11-09 16:26:46 -0500

    declared styled components w/o extending a parent

--- src/styles.d.ts
+++ src/styles.d.ts
@@ -1,5 +1,14 @@
 import "styled-components";
 
 declare module "styled-components" {
-  export interface DefaultTheme extends ITheme {}
+  // export interface DefaultTheme extends ITheme {}
+  export interface DefaultTheme {
+    core_main: string;
+    core_deep: string;
+    core_light: string;
+    core_accent: string;
+    core_sort_start: string;
+    core_sort_end: string;
+    font_family: string;
+  }
 }
