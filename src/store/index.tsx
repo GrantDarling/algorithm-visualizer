12c8ecc527261a3aa7477e9909738e64cccd94ec
Grant Darling <grantcarterdarling@gmail.com>
2021-11-14 17:47:02 -0500

    removed composewdevtools

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "./reducer";
 
 export const store = createStore(
   reducer,
-  compose(applyMiddleware(thunk), composeWithDevTools())
+  compose(applyMiddleware(thunk)) // composeWithDevTools()
 );
